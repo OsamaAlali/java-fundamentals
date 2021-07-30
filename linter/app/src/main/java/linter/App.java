21f58d5de56c1e4ea1f02c915a3de897c9641bb9
OsamaAlali <osama.alali89@yahoo.com>
2021-07-30 22:57:48 +0300

    add file test

--- linter/app/src/main/java/linter/App.java
+++ linter/app/src/main/java/linter/App.java
@@ -14,11 +14,13 @@ public class App {
     }
 
  public static String check(String link){
+
         String error="";
      int lineCount=0;
      Path path= Paths.get(link);
      try {
          List<String> lines= Files.readAllLines(path);
+         if (!(lines.size()==0)){
          for (String line : lines) {
              lineCount++;
          if ( line.isBlank() ||(line.endsWith("{")) || line.endsWith("}") || line.contains("else") ||line.contains("if") || line.endsWith(";") ){
@@ -27,16 +29,21 @@ public class App {
              error +=" Line "+String.valueOf(lineCount)+": Missing semicolon.\n";
                  }
              }
-
+         }else {
+             return "File is Empty";
+         }
      } catch (IOException e) {
          e.printStackTrace();
+
      }
 
      return error;
  }
     public static void main(String[] args) {
   String path=".\\app\\src\\main\\resources\\gates.js";
-   String error=  check(path);
+//          "resources\\gates.js";
+
+        String error=  check(path);
         System.out.println(error);
     }
 }
